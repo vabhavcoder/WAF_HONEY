8ea422b06d9db4a9f2642ad9240ef1462f5c2417
vabhavcoder <vabhavbhateja@gmail.com>
2026-09-04 17:24:13 +0530

    Change honeypot listening address to 127.0.0.1

--- run_honeypot.py
+++ run_honeypot.py
@@ -15,5 +15,5 @@ from proxy.config import HONEYPOT_PORT
 if __name__ == "__main__":
     threading.Thread(target=start_fake_ssh, daemon=True).start()
 
-    print(f" * Fake web honeypot listening on 0.0.0.0:{HONEYPOT_PORT}")
-    web_app.run(host="0.0.0.0", port=HONEYPOT_PORT, threaded=True)
+    print(f" * Fake web honeypot listening on 127.0.0.1:{HONEYPOT_PORT}")
+    web_app.run(host="127.0.0.1", port=HONEYPOT_PORT, threaded=True)
